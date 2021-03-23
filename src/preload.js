20d80082ae0aef628bda1bfdf90a0ab5de6a1f83
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2021-03-23 18:43:50 +0100

    Inital Commit Not Fixed: Semester Choose Bug

---
+++ src/preload.js
@@ -0,0 +1,24 @@
+const {
+    contextBridge,
+    ipcRenderer
+} = require("electron");
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+    "api", {
+        send: (channel, data) => {
+            // whitelist channels
+            let validChannels = ["toMain"];
+            if (validChannels.includes(channel)) {
+                ipcRenderer.send(channel, data);
+            }
+        },
+        receive: (channel, func) => {
+            ipcRenderer.on(channel, (event, args) => {
+                func(JSON.parse(args))
+                args = null;
+            });
+        }
+    }
+);
