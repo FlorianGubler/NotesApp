3df1f800de25706511e2905c691ac8a43b8d1e31
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2021-06-08 10:58:34 +0200

    Finsish stickyNotes Tool

--- src/frontend/assets/js/sleep.js
+++ src/frontend/assets/js/sleep.js
@@ -6,3 +6,8 @@ async function reloadSideAsync() {
     await sleep(1000);
     location.reload();
 }
+
+async function loadingiconAsync(){
+    await sleep(1000);
+    document.getElementById("loading-container").style.display = "none";
+}
